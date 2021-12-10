ff311f174afcecf23c824a7f458879d48938f381
son <leduyson2632001@gmail.com>
2021-12-10 21:12:28 +0700

    change login route accept json request

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from .endpoints.user import router as user_router
 from .endpoints.authentication import router as auth_router
+from .endpoints.home import router as home_router
 
 router = APIRouter()
 router.include_router(auth_router)
 router.include_router(user_router)
+router.include_router(home_router)
