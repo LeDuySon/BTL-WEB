7f52f4d519340445b491e2a92d585a9eee337afc
NgocLam <hoanglam2242001@gmail.com>
2021-12-12 10:27:14 +0700

    create survey in endpoints

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -4,10 +4,11 @@ from .endpoints.user import router as user_router
 from .endpoints.authentication import router as auth_router
 from .endpoints.home import router as home_router
 from .endpoints.location import router as location_router
+from .endpoints.survey import router as survey_router
 
 router = APIRouter()
 router.include_router(auth_router)
 router.include_router(user_router)
 router.include_router(home_router)
 router.include_router(location_router)
-
+router.include_router(survey_router)
--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -3,10 +3,6 @@ from pydantic.networks import EmailStr
 from pymongo import MongoClient
 from uuid import uuid4
 
-from app.crud.location import get_location_unit_from_location_code
-from app.crud.survey import get_citizen_by_identidy_number, get_citizens_from_survey_col
-from app.models.location import LocationFromCode
-
 from ....core.jwt import validate_token
 from ....core.authorization import (validate_role_authorization_on_create, 
                                     validate_user_authorization_on_crud_others)
@@ -110,37 +106,4 @@ def set_child_user_state(
         "success": is_success,
         "messages": {},
     }
-
-@router.get('/user/survey/location/citizens', tags=["User"])
-def get_citizens_from_location_code(
-    code: str,
-    db: MongoClient = Depends(get_database),
-    auth: AuthToken = Depends(validate_token)
-):
-    """List all citizens of a given location code"""
-    location_unit = get_location_unit_from_location_code(code)
-    data = get_citizens_from_survey_col(code, location_unit, db)
-    
-    return {
-        "success": True,
-        "messages": {
-            "data": data
-        }
-    }
-
-@router.get('/user/survey/citizen-by-id-number', tags=["User"])
-def get_citizen_by_id_number(
-    id_number: str,
-    db: MongoClient = Depends(get_database),
-    auth: AuthToken = Depends(validate_token)
-):
-    """Get one citizen by a given identity number"""
-    data = get_citizen_by_identidy_number(id_number, db)
-    
-    return {
-        "success": True,
-        "messages": {
-            "data": data
-        }
-    }  
-    
+ 
