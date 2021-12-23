ced5230d0da83fcc7413adf1bf5dc26ddb919f6a
Le Duy Son <leduyson2632001@gmail.com>
2021-12-23 07:56:10 +0700

    add route /location/create

--- app/crud/location.py
+++ app/crud/location.py
@@ -6,7 +6,7 @@ from ..core.config import (database_name,
                            city_collection_name,
                            district_collection_name,
                            ward_collection_name)
-from ..models.location import LocationInCreate
+from ..models.location import LocationInUpdateCode, LocationInCreate
 from ..models.user import User
 
 
@@ -68,8 +68,32 @@ def get_all_childs_of_location(
         return data
     return []
 
-
-def update_code_of_location(user: User, location: LocationInCreate, db: MongoClient):
+def is_location_exists(parents_code: str, location: LocationInCreate, db: MongoClient):
+    """True if exists else False"""
+    query = None 
+    if(location.code is not None):
+        query = {"code": location.code}
+    else:
+        query = {"name": location.name}
+        
+    query_collection = get_loc_name_from_parent_code(parents_code, db)
+    results = db[database_name][query_collection].find(query)
+    
+    if(results.count() == 0):
+        return False 
+    return True
+    
+def create_new_location(user: User, location: LocationInCreate, db: MongoClient):
+    query_collection = get_loc_name_from_parent_code(user.manage_location, db)
+    insert_dict = {
+        "name": location.name,
+        "code": location.code, 
+        "parents_code": user.manage_location,
+    }
+    
+    db[database_name][query_collection].insert_one(insert_dict)
+    
+def update_code_of_location(user: User, location: LocationInUpdateCode, db: MongoClient):
     query_collection = get_collection_name_from_location_code(location.code)
     data = db[database_name][query_collection].find({"code": location.code})
     data = list(data)
@@ -83,15 +107,11 @@ def update_code_of_location(user: User, location: LocationInCreate, db: MongoCli
         return True
     return False
 
-
 """
 Utils for location
 """
 
-
-def get_collection_name_from_location_code(code: str) -> str:
-    code_length = len(code)
-    collection_name = None
+def get_collection_name_from_code_length(code_length):
     if(code_length == 1):
         collection_name = country_collection_name
     elif(code_length == 2):
@@ -102,6 +122,17 @@ def get_collection_name_from_location_code(code: str) -> str:
         collection_name = ward_collection_name
     return collection_name
 
+def get_loc_name_from_parent_code(parent_code: str, db: MongoClient):
+    """Get collection name of location managed by this user"""
+    plus_length = 1 if len(parent_code) == 1 else 2 
+    code_length = len(parent_code) + plus_length
+    
+    return get_collection_name_from_code_length(code_length)
+    
+def get_collection_name_from_location_code(code: str) -> str:
+    code_length = len(code)
+    
+    return get_collection_name_from_code_length(code_length)
 
 def get_location_unit_from_location_code(code: str):
     location_unit = None
@@ -119,7 +150,7 @@ def get_location_unit_from_location_code(code: str):
 
 def get_collection_field(location_info: dict) -> str:
     """Get the sub collection in location info (country -> city)"""
-    exclude_fields = ["_id", "name", "code"]
+    exclude_fields = ["_id", "name", "code", "parents_code"]
     for key in location_info.keys():
         if(key not in exclude_fields):
             return key
