c9222c001d6169b4389ef448f5091170505ac86b
NgocLam <hoanglam2242001@gmail.com>
2021-12-26 16:46:20 +0700

    update api 15

--- app/crud/survey.py
+++ app/crud/survey.py
@@ -204,56 +204,56 @@ def retrieve_doc_in_survey(keyword: str, loc_code: str, db: MongoClient):
     return list(data)
 
 
-def transform_data(data, db):
-    hometown = db[database_name][city_collection_name].find_one(
-        {'name': data['hometown']})['code']
+# def transform_data(data, db):
+#     hometown = db[database_name][city_collection_name].find_one(
+#         {'name': data['hometown']})['code']
 
-    p_city = db[database_name][city_collection_name].find_one(
-        {'name': data['permanent_address']['city']})['code']
-    p_district = db[database_name][district_collection_name].find_one(
-        {'name': data['permanent_address']['district']})['code']
+#     p_city = db[database_name][city_collection_name].find_one(
+#         {'name': data['permanent_address']['city']})['code']
+#     p_district = db[database_name][district_collection_name].find_one(
+#         {'name': data['permanent_address']['district']})['code']
 
-    p_ward = db[database_name][ward_collection_name].find_one(
-        {'name': data['permanent_address']['ward']})['code']
-    p_civil_group = db[database_name][civil_group_collection_name].find_one(
-        {'name': data['permanent_address']['civil_group']})['code']
+#     p_ward = db[database_name][ward_collection_name].find_one(
+#         {'name': data['permanent_address']['ward']})['code']
+#     p_civil_group = db[database_name][civil_group_collection_name].find_one(
+#         {'name': data['permanent_address']['civil_group']})['code']
 
-    t_city = db[database_name][city_collection_name].find_one(
-        {'name': data['temporary_address']['city']})['code']
-    t_district = db[database_name][district_collection_name].find_one(
-        {'name': data['temporary_address']['district']})['code']
-    t_ward = db[database_name][ward_collection_name].find_one(
-        {'name': data['temporary_address']['ward']})['code']
-    t_civil_group = db[database_name][civil_group_collection_name].find_one(
-        {'name': data['temporary_address']['civil_group']})['code']
+#     t_city = db[database_name][city_collection_name].find_one(
+#         {'name': data['temporary_address']['city']})['code']
+#     t_district = db[database_name][district_collection_name].find_one(
+#         {'name': data['temporary_address']['district']})['code']
+#     t_ward = db[database_name][ward_collection_name].find_one(
+#         {'name': data['temporary_address']['ward']})['code']
+#     t_civil_group = db[database_name][civil_group_collection_name].find_one(
+#         {'name': data['temporary_address']['civil_group']})['code']
 
-    trans_data = {
-        'identity_number': data['identity_number'],
-        'fullname': data['fullname'],
-        'birthday': data['dob'],
-        'gender': data['gender'],
-        'hometown': hometown,
-        'permanent_address': {
-            'city': p_city,
-            'district': p_district,
-            'ward': p_ward,
-            'civil_group': p_civil_group,
-            'home_address': data['permanent_address']['home_address'] + ', ' + data['permanent_address']['ward'] + ', ' + data['permanent_address']['district'] + ', ' + data['permanent_address']['city']
-        },
-        'temporary_address': {
-            'city': t_city,
-            'district': t_district,
-            'ward': t_ward,
-            'civil_group': t_civil_group,
-            'home_address': data['temporary_address']['home_address'] + ', ' + data['temporary_address']['ward'] + ', ' + data['temporary_address']['district'] + ', ' + data['temporary_address']['city']
-        },
-        'religion': data['religion'],
-        'job': data['job'],
-        'edu_level': data['edu_level'],
-        'createAt': data['createAt']
-    }
+#     trans_data = {
+#         'identity_number': data['identity_number'],
+#         'fullname': data['fullname'],
+#         'birthday': data['dob'],
+#         'gender': data['gender'],
+#         'hometown': hometown,
+#         'permanent_address': {
+#             'city': p_city,
+#             'district': p_district,
+#             'ward': p_ward,
+#             'civil_group': p_civil_group,
+#             'home_address': data['permanent_address']['home_address'] + ', ' + data['permanent_address']['ward'] + ', ' + data['permanent_address']['district'] + ', ' + data['permanent_address']['city']
+#         },
+#         'temporary_address': {
+#             'city': t_city,
+#             'district': t_district,
+#             'ward': t_ward,
+#             'civil_group': t_civil_group,
+#             'home_address': data['temporary_address']['home_address'] + ', ' + data['temporary_address']['ward'] + ', ' + data['temporary_address']['district'] + ', ' + data['temporary_address']['city']
+#         },
+#         'religion': data['religion'],
+#         'job': data['job'],
+#         'edu_level': data['edu_level'],
+#         'createAt': data['createAt']
+#     }
 
-    return trans_data
+#     return trans_data
 
 
 def insert_data_into_col(data: SurveyForm, db: MongoClient):
